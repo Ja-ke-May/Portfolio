1b00a9f11067adae359f88fa0941bf9aa3aa0142
Ja-ke-May <142454161+Ja-ke-May@users.noreply.github.com>
2023-11-06 19:53:06 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+document.getElementById('youButton').addEventListener('click', function() {
+    document.getElementById('helloWorld').innerHTML = 'Hello You'
+});
+document.getElementById('meButton').addEventListener('click', function(){
+    document.getElementById('helloWorld').innerHTML = 'Hello Me'
+});
+document.getElementById('bigButton').addEventListener('click', function(){
+    document.getElementById('helloWorld').style.scale = "1.5"
+});
+document.getElementById('smallButton').addEventListener('click', function(){
+    document.getElementById('helloWorld').style.scale = "0.5"
+});
+
+document.getElementById('CVlink').addEventListener('click', function(){
+    document.getElementById('CVpassword').style.display = 'block'
+});
+
+const correctPassword = 'Password1.';
+
+document.getElementById('CVlink').addEventListener('click', (event) => {
+    event.preventDefault(); 
+    const password = prompt('Please enter the password:'); // Prompts the user to enter the password
+
+    if (password === correctPassword) {
+        const link = event.target.getAttribute('href'); // Fetches the link from the element
+        window.open(link, '_blank'); // Opens the link in a new tab
+    } else {
+        alert('Incorrect password! Access denied.');
+    }
+});
