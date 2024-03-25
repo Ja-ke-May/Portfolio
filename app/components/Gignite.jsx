f2e3306e342ec144ac021c04c7c747b04b83c6af
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-03-25 21:11:57 +0000

    styling

--- app/components/Gignite.jsx
+++ app/components/Gignite.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+const scrollToOnyx = () => {
+  const onyx = document.getElementById('onyx');
+  onyx.scrollIntoView({ behavior: 'smooth' });
+};
+
 const Gignite = () => {
   return (
-    <div className="relative mt-60 font-mono">
+    <div id="gignite" className="relative mt-60 font-mono">
+      <hr className='border-1 border-gray-600 m-4' />
       <div className="max-w-screen text-center lg:text-left p-2 lg:p-10">
         <div className="flex flex-col md:flex-row justify-center items-center">
           <div className="flex-col p-4  w-[90%] md:w-[75%]">
@@ -20,6 +26,10 @@ const Gignite = () => {
           </div>
         </div>
       </div>
+      <br />
+      <div className="flex justify-center items-center mt-6">
+                <div className="w-6 h-6 border-t-2 border-r-2 border-red-600 transform rotate-[135deg] rounded-t rounded-r" onClick={scrollToOnyx}></div>
+              </div>
     </div>
   );
 }
