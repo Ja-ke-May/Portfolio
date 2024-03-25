5d3ab7fd1c9abc976d60121e370f71bd00e2a0d3
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-03-25 00:50:43 +0000

    navbar vercel issue

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -8,13 +8,9 @@ const Navbar = () => {
       <h2 className="text-md lg:text-xl mt-1">Full-stack Web Developer</h2>
       <h3 className="absolute right-2 lg:right-4 top-4 flex flex-row text-lg lg:text-2xl">
         
-          {/* <Link href="/cv"> */}
             <span className="hover:text-red-600 mr-2">CV</span>
-          {/* </Link> */}
-          | 
-          {/* <Link href="/contact"> */}
+          
             <span className="hover:text-red-600 ml-2">Contact</span>
-          {/* </Link> */}
         
       </h3>
     </div>
