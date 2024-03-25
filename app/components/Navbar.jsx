a59e7c4f7ee9479594246d428b0dea8ba1b4f274
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-03-25 22:04:21 +0000

    cv now pdf

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
       <h2 className="text-md lg:text-xl mt-1 text-gray-500">Full-stack Web Developer</h2>
       <h3 className="absolute right-2 lg:right-4 top-4 flex flex-row text-lg lg:text-2xl">
         
-            <span className="hover:text-red-600 mr-2"><a href="/Jacob May CV.docx" download>CV</a></span>
+            <span className="hover:text-red-600 mr-2"><a href="/Jacob May CV.pdf" download>CV</a></span>
           <span className='text-gray-600'>|</span>
             <span className="hover:text-red-600 ml-2" onClick={scrollToFooter}>Contact</span>
         
