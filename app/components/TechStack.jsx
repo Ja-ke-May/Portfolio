9c75a2f80414f4233ee8fc93064a2b7cea5ed836
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-04-01 08:51:01 +0100

    mr removed and ts pr added

--- app/components/TechStack.jsx
+++ app/components/TechStack.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const TechStack = () => {
   return (
     <div className="flex flex-col justify-center m-4">
-      <div className="flex justify-center items-center max-h-40 mt-10 mr-2">
+      <div className="flex justify-center items-center max-h-40 mt-10">
         <img src="../html-logo.png" alt="HTML" title="HTML" className="max-w-40 max-h-[90px] object-cover mx-1" />
         <img src="../css-logo.png" alt="CSS" title="CSS" className="max-w-40 max-h-20 object-cover mr-2" />
         <img src="../tailwind-logo.png" alt="Tailwind CSS" title="Tailwind CSS" className="max-w-20 max-h-20 object-cover mx-1" />
         <img src="../js-logo.png" alt="JavaScript" title="JavaScript" className="max-w-40 max-h-20 object-cover mt-2" />
-        <img src="../typescript-logo.webp" alt="TypeScript" title="TypeScript" className="max-w-40 max-h-[58px] object-cover mx-1 mt-2" />
+        <img src="../typescript-logo.webp" alt="TypeScript" title="TypeScript" className="max-w-40 max-h-[58px] object-cover mx-1 mt-2 pr-1" />
       </div>
       <div className="flex justify-center items-center max-h-40 mt-10">
         <img src="../next-js-logo.png" alt="Next.js" title="Next.js" className="max-w-40 max-h-20 object-cover filter invert mx-2" />
